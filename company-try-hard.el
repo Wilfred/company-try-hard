3ebaff2d965342191b735bfd9b0e170ae0711a18
Wilfred Hughes <me@wilfred.me.uk>
2015-08-30 20:54:08 +0100

    Implementing the function.

--- company-try-hard.el
+++ company-try-hard.el
@@ -40,8 +40,46 @@
 
 ;;; Code:
 
-(defun company-try-hard ())
+(require 'dash)
+(require 'company)
 
+(defvar-local cth--last-backend-index nil
+  "The last backend used by `company-try-hard'.")
+
+;;;###autoload
+(defun company-try-hard ()
+  "Offer completions from the first backend in `company-backends' that
+offers candidates. If called again, use the next backend, and so on."
+  (interactive)
+  ;; If the last command wasn't `company-try-hard', reset the index so
+  ;; we start at the first backend.
+  (unless (eq last-command 'company-try-hard)
+    (setq cth--last-backend-index nil))
+  ;; Close company if it's active, so `company-begin-backend' works.
+  (company-abort)
+  (catch 'break
+    ;; Try every backend until we find one that returns some
+    ;; candidates.
+    (--map-indexed
+     (catch 'continue
+       ;; If we've tried this backend before, do nothing.
+       (when (and (numberp cth--last-backend-index)
+                  (<= it-index cth--last-backend-index))
+         (throw 'continue nil))
+       ;; Try this backend, ignoring a 'no completions here' error.
+       (when (ignore-errors (company-begin-backend it))
+         ;; We've found completions here, so remember this backend and
+         ;; terminate for now.
+         (message "company-try-harder: using %s" it)
+         (setq cth--last-backend-index it-index)
+         (throw 'break nil)))
+     company-backends)
+    ;; If we haven't thrown 'break at this point, enable the user to
+    ;; cycle through again.
+    (setq cth--last-backend-index nil)))
+
+(global-set-key (kbd "C-z") #'company-try-hard)
+(define-key company-active-map (kbd "C-z") #'company-try-hard)
 
 (provide 'company-try-hard)
 ;;; company-try-hard.el ends here
