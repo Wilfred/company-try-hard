c12f20ad161f58c6c0ad05bbbeb7a061c9b0feb4
Wilfred Hughes <me@wilfred.me.uk>
2015-08-30 21:30:57 +0100

    Running checkdoc.

--- company-try-hard.el
+++ company-try-hard.el
@@ -23,7 +23,7 @@
 ;;; Commentary:
 ;;
 ;; A `company-complete' alternative that tries much harder to find
-;; completions. If none of the current completions look good, call the
+;; completions.  If none of the current completions look good, call the
 ;; command again to try the next backend.
 ;;
 ;;; Usage:
